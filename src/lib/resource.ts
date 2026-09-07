c0e5db8659d7e195addd02588b6785e582fd8dea
anujyadav70750 <anujyadav70750@gmail.com>
2026-09-07 08:39:04 +0530

    Add shared resource URL and image helpers

---
+++ src/lib/resource.ts
@@ -0,0 +1,19 @@
+import type { CollectionEntry } from 'astro:content';
+
+export const PLACEHOLDER_IMAGE = '/images/placeholder.svg';
+
+export function resourceSlug(resource: CollectionEntry<'resources'>) {
+  return resource.data.slug || resource.id;
+}
+
+export function resourceThumbnail(resource: CollectionEntry<'resources'>) {
+  return resource.data.thumbnail === PLACEHOLDER_IMAGE && resource.data.resultImages.length > 0
+    ? resource.data.resultImages[0]
+    : resource.data.thumbnail;
+}
+
+export function resourceHeroImage(resource: CollectionEntry<'resources'>) {
+  return resource.data.heroImage === PLACEHOLDER_IMAGE && resource.data.resultImages.length > 0
+    ? resource.data.resultImages[0]
+    : resource.data.heroImage;
+}
