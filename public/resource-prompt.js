590690c6a7121f8a8861438d8a3c57963ea65bd1
anujyadav70750 <anujyadav70750@gmail.com>
2026-09-16 00:37:02 +0530

    Restore existing workflow renderer before universal Step 01 work

--- public/resource-prompt.js
+++ public/resource-prompt.js
@@ -18,9 +18,7 @@
   };
 
   makeGuideInstruction();
-  import('/resource-workflow-data-bridge.js?v=20260916-1')
-    .then(() => import('/resource-workflow-v10.js?v=20260916-1'))
-    .catch(() => {
-      document.querySelector('.workflow-section.sf-v7')?.classList.add('sf-workflow-ready');
-    });
+  import('/resource-workflow-v9.js?v=20260916-1').catch(() => {
+    document.querySelector('.workflow-section.sf-v7')?.classList.add('sf-workflow-ready');
+  });
 })();
